fac63532de9836f703c269ebf9c8fbf8177989a1
rwbysafire <tomtan117@gmail.com>
2015-10-12 21:59:54 -0700

    First Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate()
+	{
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Quaternion rotation = Quaternion.LookRotation(transform.position - mousePosition, 
+		                                              Vector3.forward);
+		transform.rotation = rotation;
+		transform.eulerAngles = new Vector3(0,0, transform.eulerAngles.z);
+		GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+		if (Input.GetMouseButton(0))
+			GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed);
+	}
+
+}
