a71e6b4dd96bedfc8dad15694f3774448cad62fa
Rekkuujin <anthonyha1996@gmail.com>
2015-10-19 15:56:08 -0700

    Commit to master
    char control and basic camera

---
+++ Assets/Scripts/Cameras.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameras : MonoBehaviour {
+
+	// Script will make the camera follow the player except for rotation
+    // May need changes depending on how we decide the camera should work
+	void Update ()
+    {
+        transform.position = new Vector3 (GameObject.FindWithTag("Player").transform.position.x, GameObject.FindWithTag("Player").transform.position.y,-10);
+	}
+}
