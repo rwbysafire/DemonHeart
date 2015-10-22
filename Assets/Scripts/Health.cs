81a20f740f661194f0074ce5cb7e0dc4a02582e8
BrianLam4101 <BrianLam4101@gmail.com>
2015-10-22 15:06:27 -0700

    Updated Health UI
    Now shows the value of health in numbers as well as the ball.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class Health : MonoBehaviour {
 
-	public int health;
+	public int maxHealth, health;
+
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;
+	}
 
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/UI_Health.cs
+++ Assets/Scripts/UI_Health.cs
@@ -3,17 +3,23 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class UI_Health : MonoBehaviour {
-
-	public Slider slider;
+	
+	GameObject player;
+	Slider slider;
+	Text text;
 
 	// Use this for initialization
 	void Start () {
-	
+		slider = GetComponent<Slider> ();
+		text = GetComponentInChildren<Text> ();
+		player = GameObject.FindWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameObject.FindWithTag ("Player"))
-			slider.value = GameObject.FindWithTag ("Player").GetComponent<Health> ().health;
+		if (GameObject.FindWithTag ("Player")) {
+			slider.value = player.GetComponent<Health> ().health;
+			text.text = "Life: " + (player.GetComponent<Health>().health).ToString() + " / " + (player.GetComponent<Health>().maxHealth).ToString();
+		}
 	}
 }
