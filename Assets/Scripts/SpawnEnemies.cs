b09dcc119cfaffc6bbe3fd6e86b4be35db4a5bc1
BrianLam4101 <BrianLam4101@gmail.com>
2015-10-22 19:48:03 -0700

    Revert "Revert "Added one skill and small other changes""
    This reverts commit 0ad14fb4de973efb730eceb114dc270c4a2a1df7.

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range (1, 30) > GameObject.FindGameObjectsWithTag ("Enemy").GetLength (0)) {
+			GameObject enemy = Instantiate (Resources.Load ("Enemy")) as GameObject;
+			enemy.transform.position = new Vector3 (Random.Range (-10, 10), Random.Range (-10, 10), 0).normalized * Random.Range (10, 15);
+		}
+	}
+}
