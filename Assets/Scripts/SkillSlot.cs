dad817fbb70abc63ae077955a4701a02041cc5a1
BrianLam4101 <BrianLam4101@gmail.com>
2015-10-25 22:02:29 -0700

    Updated HUD and Ennemy spawns
    - in the process of connecting skills to the HUD without putting them on
    the HUD
    - made enemy spawns originate from around the player

---
+++ Assets/Scripts/SkillSlot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SkillSlot : MonoBehaviour {
+	
+	public KeyCode keyBind;
+	Slider slider;
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		text = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void setCooldown (float cooldown) {
+		slider.maxValue = cooldown;
+	}
+
+	public void displayCooldown (float remainingCD) {
+		slider.value = remainingCD;
+		if (remainingCD >= 0)
+			text.text = ((int)remainingCD + 1).ToString ();
+		else
+			text.text = "";
+	}
+}
--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -12,7 +12,7 @@ public class SpawnEnemies : MonoBehaviour {
 	void Update () {
 		if (Random.Range (1, 30) > GameObject.FindGameObjectsWithTag ("Enemy").GetLength (0)) {
 			GameObject enemy = Instantiate (Resources.Load ("Enemy")) as GameObject;
-			enemy.transform.position = new Vector3 (Random.Range (-10, 10), Random.Range (-10, 10), 0).normalized * Random.Range (10, 15);
+			enemy.transform.position = new Vector3 (Random.Range (-10, 10), Random.Range (-10, 10), 0).normalized * Random.Range (10, 15) + transform.position;
 		}
 	}
 }
