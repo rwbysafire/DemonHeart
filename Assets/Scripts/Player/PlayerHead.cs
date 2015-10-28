1d2d87fe88a70b2889d33f32816b05d03803bae0
BrianLam4101 <BrianLam4101@gmail.com>
2015-10-28 16:11:34 -0700

    ---work in progress--- Created a new Player
    - Created new player movement, that removes the problem of moving when
    walking diagonaly.
    - Changed the input settings to give the player a little over twice the
    acceleration, and doesnt allow the player to instantly change
    directions.
    - Created my own test scene to test my changes

---
+++ Assets/Scripts/Player/PlayerHead.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHead : MonoBehaviour {
+	
+	public Skill teleport;
+	public Skill scattershot;
+	public Skill powershot;
+	public Skill basicAttack;
+	
+	void Start()
+	{
+		teleport = new SkillTeleport (GameObject.FindWithTag ("Player"));
+		scattershot = new SkillScattershot (GameObject.Find ("Head"));
+		powershot = new SkillPowershot (GameObject.Find ("Head"));
+		basicAttack = new SkillBasicAttack (GameObject.Find ("Head"));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = GameObject.Find ("Feet").transform.position;
+		Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		diff.Normalize();
+		
+		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+		
+		if (Input.GetKey (KeyCode.E)) 
+		{
+			teleport.useSkill();
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+		}
+		//scattershot.useSkill ();
+		if (Input.GetKey (KeyCode.R))
+			powershot.useSkill (); 
+		if (Input.GetKey (KeyCode.Mouse0))
+			basicAttack.useSkill ();
+	}
+}
