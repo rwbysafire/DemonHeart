99ff588216442dbc87b44ae72698731af2d98bbd
GDFauxtrot <gregduarte97@gmail.com>
2015-10-31 13:53:42 -0700

    Skill Commit
    Having problems with GitHub Desktop. :I

--- Assets/Scripts/Skills/SkillBasicAttack.cs
+++ Assets/Scripts/Skills/SkillBasicAttack.cs
@@ -16,8 +16,8 @@ public class SkillBasicAttack : Skill
 	public override void skillLogic() {
 		//Instantiates the projectile with some speed
 		GameObject basicArrow = MonoBehaviour.Instantiate(Resources.Load("Arrow_Placeholder")) as GameObject;
-		basicArrow.GetComponent<basic_projectile>().speed = 10;
-		basicArrow.GetComponent<basic_projectile>().damage = 10;
+		//basicArrow.GetComponent<basic_projectile>().speed = 10;
+		//basicArrow.GetComponent<basic_projectile>().damage = 10;
 		//Initiates the projectile's position and rotation
 		basicArrow.transform.position = this.getGameObject().transform.position;
 		basicArrow.transform.rotation = this.getGameObject().transform.rotation;
