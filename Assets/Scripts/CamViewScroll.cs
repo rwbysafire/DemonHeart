dbaca00ea18e7a3df0d2aa8b8c90ebf2725e32a4
GDFauxtrot <gregduarte97@gmail.com>
2015-11-17 00:29:14 -0800

    UITests
    Created CamViewScroll and new UI design, implemented in UITests.unity.
    Tweaked HealthUI to work with new design (apologies if this destroys a
    system in another project).

---
+++ Assets/Scripts/CamViewScroll.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamViewScroll : MonoBehaviour {
+/// <summary>
+/// When attached to the main camera in the scene, enables the user to use the scroll wheel on the mouse to zoom in/out.
+/// </summary>
+
+	[Header("Recommended: ~4")]
+	public int minimumZoom;
+	[Header("Unity Default: 5")]
+	public int defaultZoom;
+	[Header("Recommended: ~8")]
+	public int maximumZoom;
+
+	private int currentZoom;
+
+	private Camera cam;
+
+	void Start () {
+		currentZoom = defaultZoom;
+		cam = Camera.main;
+	}
+
+	void Update () {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+			cam.orthographicSize -= 1;
+			if (cam.orthographicSize < minimumZoom) {
+				cam.orthographicSize = minimumZoom;
+			}
+		} else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+			cam.orthographicSize += 1;
+			if (cam.orthographicSize > maximumZoom) {
+				cam.orthographicSize = maximumZoom;
+			}
+		}
+	}
+}
