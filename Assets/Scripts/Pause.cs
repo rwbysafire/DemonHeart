1e017358cf9e74b78ca3112f9116aa844e3c3f0e
BrianLam4101 <BrianLam4101@gmail.com>
2015-11-25 23:50:35 -0800

    Updated Pause Menu
    - Updated Pause functionality to have a window
    - Added screen fade to black when game paused
    - Added a respawn button on the pause menu
    - Added place holder buttons on pause menu that I will implement later

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Pause : MonoBehaviour {
     bool showText = false;
-    Rect textArea = new Rect(0, 0, Screen.width, Screen.height);
+	Rect textArea = new Rect(0, 0, Screen.width, Screen.height);
+	Image fade;
+	GameObject window;
     //Shows a GUI in top right when Esc is pressed once saying "PAUSED", should change this when pause menu is finished
+
+	void Start() {
+		window = transform.GetChild(0).gameObject;
+		fade = GetComponent<Image>();
+	}
+
     void OnGUI()
     {
         if(showText == true)
@@ -18,13 +27,24 @@ public class Pause : MonoBehaviour {
             {//stops the game when you press Esc, sets showText = true
                 showText = true;
                 Time.timeScale = 0;
+				fade.enabled = true;
+				window.SetActive(true);
             }
             else
             {//continues the game when pressing Esc while still paused, makes "PAUSED" disappear
                 showText = false;
                 Time.timeScale = 1;
+				fade.enabled = false;
+				window.SetActive(false);
             }
         }
 	
+	}
+
+	public void respawnPlayer() {
+		if(GameObject.Find("Player"))
+			Destroy(GameObject.Find("Player"));
+		GameObject player = Instantiate(Resources.Load<GameObject>("Player"));
+		player.name = "Player";
 	}
 }
