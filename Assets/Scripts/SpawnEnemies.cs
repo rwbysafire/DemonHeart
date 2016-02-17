94f5e2101eed03511c3f99bb2db07d6f515da8e6
PETEroid <selllfocard@gmail.com>
2016-02-16 17:18:30 -0800

    fix wave system

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class SpawnEnemies : MonoBehaviour {
 
-	public int maxEnemies = 50;
-    public Wave[] waves;
+	public CanvasGroup displayText;
     public int currentWave = 0;
+
+	private Wave[] waves;
+	private bool isSpawning = false;
 
 	// Use this for initialization
 	void Start () {
         waves = new Wave[] {
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 10),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 20),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") , Resources.Load<GameObject>("Kamikaze") }, 10),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 40),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 40),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 40),
+            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 5),
+            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 5),
+            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") , Resources.Load<GameObject>("Kamikaze") }, 5),
+            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 5),
+            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 5),
+            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 5),
             new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 40),
             new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 40),
             new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 40),
@@ -27,14 +30,28 @@ public class SpawnEnemies : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (waves[currentWave].count == 0 && currentWave < waves.Length - 1) {
+		// need to fix, this method is slow (?)
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		if (enemies.Length == 0 && currentWave < waves.Length - 1 && !isSpawning) {
             currentWave += 1;
             StartCoroutine("spawnEnemy", waves[currentWave]);
-            print("Starting next wave");
+			print ("Starting wave: " + currentWave.ToString ());
         }
 	}
-
+		
     IEnumerator spawnEnemy(Wave wave) {
+		isSpawning = true;
+
+		// wait some seconds before start
+		displayText.alpha = 1f;
+		float secondsToWait = 2f;
+		float step = 0.025f;
+		while (displayText.alpha > 0) {
+			displayText.alpha = Mathf.Clamp01 (displayText.alpha - step);
+			yield return new WaitForSeconds(secondsToWait * step);
+		}
+		yield return new WaitForSeconds(0.5f);
+
         while (wave.count > 0) {
             GameObject enemy = Instantiate<GameObject>(wave.enemies[Random.Range(0, wave.enemies.Length)]);
             wave.count -= 1;
@@ -42,6 +59,7 @@ public class SpawnEnemies : MonoBehaviour {
             enemy.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0, 360));
             yield return new WaitForSeconds(0.5f);
         }
+		isSpawning = false;
     }
 }
 
