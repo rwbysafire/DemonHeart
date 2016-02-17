f55b0111a38d493f94ed9a86be915c317069b2e8
PETEroid <selllfocard@gmail.com>
2016-02-16 18:06:39 -0800

    put waves design in json file

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -1,40 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using SimpleJSON;
 
 public class SpawnEnemies : MonoBehaviour {
 
 	public CanvasGroup displayText;
     public int currentWave = 0;
+	public TextAsset waveFile;
 
-	private Wave[] waves;
+	private List<Wave> waveList;
 	private bool isSpawning = false;
 
 	// Use this for initialization
 	void Start () {
-        waves = new Wave[] {
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 5),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 5),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") , Resources.Load<GameObject>("Kamikaze") }, 5),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 5),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 5),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 5),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 40),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 40),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 40),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Zombie") }, 40),
-            new Wave(new GameObject[] { Resources.Load<GameObject>("Kamikaze") }, 40),
-        };
-        StartCoroutine("spawnEnemy", waves[currentWave]);
+		// load the wave file
+		waveList = new List<Wave>();
+		JSONNode wavesData = JSON.Parse(waveFile.text);
+		JSONArray wavesName = wavesData ["waves"].AsArray;
+		for (int i = 0; i < wavesName.Count; i++) {
+			JSONNode waveData = wavesData [wavesName [i]];
+			int count = waveData ["count"].AsInt;
+			JSONArray enemies = waveData ["enemies"].AsArray;
+			GameObject[] enemiesGameObject = new GameObject[enemies.Count];
+			for (int j = 0; j < enemiesGameObject.Length; j++) {
+				enemiesGameObject [j] = Resources.Load<GameObject> (enemies[j]);
+			}
+			waveList.Add (new Wave(enemiesGameObject, count));
+		}
+		Debug.Log (waveList.Count.ToString () + " waves loaded");
+
+		StartCoroutine("spawnEnemy", waveList[currentWave]);
     }
 	
 	// Update is called once per frame
 	void Update () {
 		// need to fix, this method is slow (?)
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
-		if (enemies.Length == 0 && currentWave < waves.Length - 1 && !isSpawning) {
+		if (enemies.Length == 0 && currentWave < waveList.Count - 1 && !isSpawning) {
             currentWave += 1;
-            StartCoroutine("spawnEnemy", waves[currentWave]);
+            StartCoroutine("spawnEnemy", waveList[currentWave]);
 			print ("Starting wave: " + currentWave.ToString ());
         }
 	}
