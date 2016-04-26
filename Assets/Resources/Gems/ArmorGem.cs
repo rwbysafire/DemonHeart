b6ad0af9f9a0ce4bb656a4437ad1ee3911caea95
PETEroid <selllfocard@gmail.com>
2016-04-26 16:26:23 -0700

    delete commented codes in armor gem script

--- Assets/Resources/Gems/ArmorGem.cs
+++ Assets/Resources/Gems/ArmorGem.cs
@@ -11,30 +11,6 @@ public class ArmorGem : ArmorItem {
 
 	// override this in the new class to give the buff
 	protected virtual void InitBuff () {
-
-		// random gen a gem
-//		switch (Random.Range (0, 5)) {
-//		case 0:
-//			buff.baseHealth = Random.Range (100, 300);
-//			this.itemDescription = "Health +" + buff.baseHealth.ToString ();
-//			break;
-//		case 1:
-//			buff.baseMana = Random.Range (100, 300);
-//			this.itemDescription = "Mana +" + buff.baseMana.ToString ();
-//			break;
-//		case 2:
-//			buff.strength = Random.Range (10, 50);
-//			this.itemDescription = "Strength +" + buff.strength.ToString ();
-//			break;
-//		case 3:
-//			buff.intelligence = Random.Range (10, 50);
-//			this.itemDescription = "Intelligence +" + buff.intelligence.ToString ();
-//			break;
-//		case 4:
-//			buff.dexterity = Random.Range (10, 50);
-//			this.itemDescription = "Dexterity +" + buff.dexterity.ToString ();
-//			break;
-//		}
 	}
 
 	public Buff GetBuff () {
