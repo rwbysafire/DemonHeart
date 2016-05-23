154add9b109c82c83a8cd10f4fc4cf2c2485a8be
haiiitao <haiiitaooo.z@gmail.com>
2016-05-22 23:25:06 -0700

    fixed skills picking
    fixed that skill can be picked again if it was unpicked

--- Assets/Scripts/ChangeSkillButton.cs
+++ Assets/Scripts/ChangeSkillButton.cs
@@ -1,38 +1,1 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-
-public class ChangeSkillButton : MonoBehaviour {
-
-	public int index;
-	bool mark = false;
-//	Dictionary<int, string> skills = new Dictionary<int, string>();
-
-	void Update () 
-	{
-		if (index == 0 && !mark)
-		{
-			if (GameObject.Find("Player")) 
-			{
-				Skill skill = GameObject.Find("Player").GetComponent<Mob>().skills[index];
-				GetComponent<Image>().sprite =skill.getImage();
-				GetComponentInChildren<Text>().text = skill.getName();
-//				skills.Add(index, skill.getName());
-			}
-			mark = true;
-		}
-
-		if (GameObject.Find("Player") && index != 0) {
-			Skill skill = GameObject.Find("Player").GetComponent<Mob>().skills[index];
-//			if (skills.ContainsValue(skill.getName()) == false)
-//			{
-			if (skill != null) {
-				GetComponent<Image> ().sprite = skill.getImage ();
-				GetComponentInChildren<Text> ().text = skill.getName ();
-			}
-//				skills.Add(index, skill.getName());
-//			}
-		}
-	}
-}
+﻿using UnityEngine; using UnityEngine.UI; using System.Collections; using System.Collections.Generic;  public class ChangeSkillButton : MonoBehaviour {  	public int index;	// changeskillbutton 	bool mark = false;  	void Update ()  	{ 		if (index == 0 && mark == false) 		{ 			if (GameObject.Find("Player"))  			{ 				Skill skill = GameObject.Find("Player").GetComponent<Mob>().skills[index]; 				GetComponent<Image>().sprite =skill.getImage(); 				GetComponentInChildren<Text>().text = skill.getName(); 			} 			mark = true; 		}  		if (GameObject.Find("Player") && index != 0) { 			Skill skill = GameObject.Find("Player").GetComponent<Mob>().skills[index]; 			if (skill != null) { 				GetComponent<Image> ().sprite = skill.getImage (); 				GetComponentInChildren<Text> ().text = skill.getName (); 			} 		} 	} }
--- Assets/Scripts/PickSkill.cs
+++ Assets/Scripts/PickSkill.cs
@@ -1,37 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class PickSkill : MonoBehaviour {
-
-	int index;
-	Dictionary<int, bool> mark = new Dictionary<int, bool> (); 
-//	Skill[] skills;
-//
-//	void Update () {
-//		if (Input.GetKeyUp(KeyCode.V))
-//			gameObject.SetActive(false);
-//	}
-	void Start()
-	{
-		mark [0] = true;
-		for (int i = 1; i < 11; i++) 
-		{
-			mark [i] = false;
-		}
-	}
-
-	public void setIndex(int i) 
-	{
-		index = i;
-	}
-
-	public void replaceSkill(int i) {
-		if (mark [i] == false) {
-			if (GameObject.Find ("Player")) {
-				GameObject.Find ("Player").GetComponent<Mob> ().replaceSkill (index, GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i]);
-				mark [i] = true;
-			}
-		}
-	}
-}
+﻿using UnityEngine; using System.Collections; using System.Collections.Generic;  public class PickSkill : MonoBehaviour {  	int index; 	// 	//	void Update () { 	//		if (Input.GetKeyUp(KeyCode.V)) 	//			gameObject.SetActive(false); 	//	}   	public void setIndex(int i)  	{ 		index = i; // changeskillbutton 	}  	public void replaceSkill(int i)  // skillbutton 	{ 		if (GameObject.Find ("Player") && index != 0 && i != 0) { 			bool mark = false; 			for (int j = 0; j < 6; j++) 			{ 				if (GameObject.Find ("Player").GetComponent<Mob> ().skills [j] != null) { 					if (GameObject.Find ("Player").GetComponent<Mob> ().skills [j].getName () == GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i].getName ()) 						mark = true; 					else 						mark = false; 				} 			} 			if (mark == false) 				GameObject.Find ("Player").GetComponent<Mob> ().replaceSkill (index, GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i]); 		} 	} }
