2494ecfc0cb3e140c4a60f01a9a5e8c5d107f119
haiiitao <haiiitaooo.z@gmail.com>
2016-05-22 23:54:06 -0700

    Updated
    Sorry that script format was wrong for the first commit. I got parser
    error for the first commit, and I fixed it this time.

--- Assets/Scripts/ChangeSkillButton.cs
+++ Assets/Scripts/ChangeSkillButton.cs
@@ -1,1 +1,34 @@
-﻿using UnityEngine; using UnityEngine.UI; using System.Collections; using System.Collections.Generic;  public class ChangeSkillButton : MonoBehaviour {  	public int index;	// changeskillbutton 	bool mark = false;  	void Update ()  	{ 		if (index == 0 && mark == false) 		{ 			if (GameObject.Find("Player"))  			{ 				Skill skill = GameObject.Find("Player").GetComponent<Mob>().skills[index]; 				GetComponent<Image>().sprite =skill.getImage(); 				GetComponentInChildren<Text>().text = skill.getName(); 			} 			mark = true; 		}  		if (GameObject.Find("Player") && index != 0) { 			Skill skill = GameObject.Find("Player").GetComponent<Mob>().skills[index]; 			if (skill != null) { 				GetComponent<Image> ().sprite = skill.getImage (); 				GetComponentInChildren<Text> ().text = skill.getName (); 			} 		} 	} }
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangeSkillButton : MonoBehaviour 
+{
+	public int index;
+	bool mark = false;
+
+	void Update()
+	{
+		if (index == 0 && mark == false) 
+		{
+			if (GameObject.Find ("Player"))
+			{
+				Skill skill = GameObject.Find ("Player").GetComponent<Mob> ().skills [index];
+				GetComponent<Image> ().sprite = skill.getImage ();
+				GetComponentInChildren<Text> ().text = skill.getName ();
+			}
+			mark = true;
+		}
+
+		if (GameObject.Find ("Player") && index != 0)
+		{
+			Skill skill = GameObject.Find ("Player").GetComponent<Mob> ().skills [index];
+			if (skill != null) 
+			{
+				GetComponent<Image> ().sprite = skill.getImage ();
+				GetComponentInChildren<Text> ().text = skill.getName ();
+			}
+		}
+	}
+}
--- Assets/Scripts/PickSkill.cs
+++ Assets/Scripts/PickSkill.cs
@@ -1,1 +1,35 @@
-﻿using UnityEngine; using System.Collections; using System.Collections.Generic;  public class PickSkill : MonoBehaviour {  	int index; 	// 	//	void Update () { 	//		if (Input.GetKeyUp(KeyCode.V)) 	//			gameObject.SetActive(false); 	//	}   	public void setIndex(int i)  	{ 		index = i; // changeskillbutton 	}  	public void replaceSkill(int i)  // skillbutton 	{ 		if (GameObject.Find ("Player") && index != 0 && i != 0) { 			bool mark = false; 			for (int j = 0; j < 6; j++) 			{ 				if (GameObject.Find ("Player").GetComponent<Mob> ().skills [j] != null) { 					if (GameObject.Find ("Player").GetComponent<Mob> ().skills [j].getName () == GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i].getName ()) 						mark = true; 					else 						mark = false; 				} 			} 			if (mark == false) 				GameObject.Find ("Player").GetComponent<Mob> ().replaceSkill (index, GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i]); 		} 	} }
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickSkill : MonoBehaviour 
+{
+
+	int index;
+
+	public void setIndex(int i)
+	{
+		index = i;
+	}
+
+	public void replaceSkill(int i)
+	{
+		if (GameObject.Find ("Player") && index != 0 && i != 0)
+		{
+			bool mark = false;
+			for (int j = 0; j < 6; j++)
+			{
+				if (GameObject.Find ("Player").GetComponent<Mob> ().skills [j] != null)
+				{
+					if (GameObject.Find ("Player").GetComponent<Mob> ().skills [j].getName () == GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i].getName ())
+						mark = true;
+					else
+						mark = false;
+				}
+			}
+			if (mark == false)
+				GameObject.Find ("Player").GetComponent<Mob> ().replaceSkill (index, GameObject.Find ("Player").GetComponent<Player> ().listOfSkills [i]);
+		}
+	}
+}
+	
